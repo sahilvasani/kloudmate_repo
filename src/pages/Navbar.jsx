6251b9c71be59f2a7bb4363ba0763d748db076d3
SahilVasani3012 <98374731+sahilvasani@users.noreply.github.com>
2023-07-03 10:06:23 +0530

    Update Navbar.jsx

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -13,7 +13,7 @@ export const Navbar = () => {
     <div className="header">
       <div className="tabs-label-main-div" style={{ width: "295px" }}>
         {leftArrow()}
-        <span className="tabs-top-label-one">DELETE</span>
+        <span className="tabs-top-label-one" style={{color:"rgb(251, 49, 87)"}}>DELETE</span>
         <span className="tabs-top-label-second">/purchase-order</span>
       </div>
       <div class="searchbar">
